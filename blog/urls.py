ac3ef1a55be75c4d933c9ea6b1b3bc62b83eb37b
guli2103 <gulbahorcoder@gmail.com>
2022-11-22 19:31:34 +0500

    meta

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('main.urls')),
     path('gul/',include('gul.urls')),
     path('institut/',include('institut.urls')),
     path('moshin/', include('moshin.urls')),
